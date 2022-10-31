958b942aa544564e3a3feaf62fde0ec5f7749585
Caexar <99301731+caexar@users.noreply.github.com>
2022-10-30 22:35:54 -0500

    Delete Curso Udemy - Fernando Herrera/01-conceptos-basicos directory

--- Curso Udemy - Fernando Herrera/01-conceptos-basicos/app02.js
+++
@@ -1,5 +0,0 @@
-const saludar = (nombre) => {
-    return `saludos ${nombre}`; //gracias a las ` ` podemos concadenar la variable adentr; sino seria "saludos"+ nombre;
-};
-
-console.log(saludar(`cesar`));
